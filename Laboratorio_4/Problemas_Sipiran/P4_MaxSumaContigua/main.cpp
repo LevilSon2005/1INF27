48a8d10f793643e92cc9751957dcafda2ab3daa5
LevilSon2005 <bbromero@pucp.edu.pe>
2025-11-11 16:54:14 -0500

    DV - Maxima sumas continuas

--- Laboratorio_4/Problemas_Sipiran/P4_MaxSumaContigua/main.cpp
+++ Laboratorio_4/Problemas_Sipiran/P4_MaxSumaContigua/main.cpp
@@ -12,23 +12,24 @@ using namespace std;
 /// Salida: 7 (Los elementos que suman el máximo son 6, -2, -3, 1, 5)
 
 int max(int a, int b) {
-    return a < b ? a : b;
+    return a > b ? a : b;
 }
 
 int hallarMaxSumaCentro(int* arreglo, int medio, int inicio, int fin) {
     int derecha = 0, izquierda = 0;
     int suma_max_izq= INT_MIN, suma_max_derecha = INT_MIN;
-    for (int i=medio;i>=0;i--) {
+    for (int i=medio;i>=inicio;i--) {
         izquierda += arreglo[i];
         if (izquierda > suma_max_izq) suma_max_izq = izquierda;
     }
 
-    for (int i=medio+1;i<fin;i++) {
+    for (int i=medio+1;i<=fin;i++) {
         derecha += arreglo[i];
         if (derecha > suma_max_derecha) suma_max_derecha = derecha;
     }
 
     return suma_max_izq + suma_max_derecha;
+    // return max(max(suma_max_izq, suma_max_derecha),suma_max_izq + suma_max_derecha);
 }
 
 int hallarSumaMax(int * arreglo, int inicio, int fin) {
@@ -38,7 +39,7 @@ int hallarSumaMax(int * arreglo, int inicio, int fin) {
     int medio = (inicio +  fin)/2;
     int sumaMax_izq = hallarSumaMax(arreglo, inicio, medio);
     cout << "la suma maxima izquierda " << sumaMax_izq << endl;
-    int sumaMax_der = hallarSumaMax(arreglo, medio, fin);
+    int sumaMax_der = hallarSumaMax(arreglo, medio+1, fin);
     cout << "la suma maxima derecha " << sumaMax_der << endl;
     int sumaMax_centro = hallarMaxSumaCentro(arreglo, medio, inicio, fin);
     cout << "la suma maxima del centro " << sumaMax_centro << endl;
@@ -47,7 +48,7 @@ int hallarSumaMax(int * arreglo, int inicio, int fin) {
 }
 
 int main() {
-    int arreglo[] = {-2, -5, 6, -2, -3, 1, 5, -6};
+    int arreglo[] = {-2, -5, 6, -2, -3, -1, 5, -6};
     int n = sizeof(arreglo) / sizeof(arreglo[0]);
 
     int sumMax = hallarSumaMax(arreglo, 0, n-1);
